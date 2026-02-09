b2174982ca97da2b1dcf3acab5665c0e8a31d47c
Chris Bale <chris.bale@cscs.global>
2026-02-09 15:36:11 +0100

    vit-issue

--- detectors/models_vit.py
+++ detectors/models_vit.py
@@ -15,24 +15,27 @@ import timm.models.vision_transformer
 
 class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
     """ Vision Transformer with support for global average pooling
+    Compatible with timm >= 0.9.0
     """
     def __init__(self, global_pool=False, **kwargs):
-        super(VisionTransformer, self).__init__(**kwargs)
+        # For timm >= 0.9.0, pass empty string to disable parent's pooling
+        # We'll handle pooling ourselves in forward_features
+        super(VisionTransformer, self).__init__(global_pool='', **kwargs)
 
-        self.global_pool = global_pool
-        if self.global_pool:
+        # Store our own global_pool flag (can be True/False)
+        self.use_global_pool = global_pool
+        if self.use_global_pool:
             norm_layer = kwargs['norm_layer']
             embed_dim = kwargs['embed_dim']
             self.fc_norm = norm_layer(embed_dim)
-
-            del self.norm  # remove the original norm
+            # Don't delete self.norm - parent may not have it in newer timm
 
     def forward_features(self, x, **kwargs):
         # Note: **kwargs added for timm>=0.9.0 compatibility (accepts attn_mask, etc.)
         B = x.shape[0]
         x = self.patch_embed(x)
 
-        cls_tokens = self.cls_token.expand(B, -1, -1)  # stole cls_tokens impl from Phil Wang, thanks
+        cls_tokens = self.cls_token.expand(B, -1, -1)
         x = torch.cat((cls_tokens, x), dim=1)
         x = x + self.pos_embed
         x = self.pos_drop(x)
@@ -40,7 +43,7 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
         for blk in self.blocks:
             x = blk(x)
 
-        if self.global_pool:
+        if self.use_global_pool:
             x = x[:, 1:, :].mean(dim=1)  # global pool without cls token
             outcome = self.fc_norm(x)
         else:
@@ -48,6 +51,12 @@ class VisionTransformer(timm.models.vision_transformer.VisionTransformer):
             outcome = x[:, 0]
 
         return outcome
+
+    def forward(self, x):
+        # Override forward completely to bypass timm's forward_head pooling
+        x = self.forward_features(x)
+        x = self.head(x)
+        return x
 
 
 def vit_small_patch16(**kwargs):
